19b085e8f446e61213ea6bcefcf039792d30436c
liz-areizaga <larei002@ucr.edu>
2023-08-09 21:21:59 -0700

    chatGPT API connected and successful
    Succesfully made POST request to openAI's API.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,19 +2,24 @@ import { Configuration, OpenAIApi } from "openai";
 import dotenv from 'dotenv';
 
 dotenv.config({path: "src/.env"});
-async function listModels(): Promise<void> {
+async function connectAPI(): Promise<void> {
   try {
     const configuration = new Configuration({
       apiKey: process.env.OPENAI_API_KEY,
     });
     const openai = new OpenAIApi(configuration);
     
-    const response = await openai.listModels();
+    let toSummarize: string = "Addie had lived in Happyville since she was born. Next week, however, Addie and her family were moving over 1,000 miles away to Washington. Addie despised the idea of moving for many reasons. She was sad to be leaving her best friend. She had played on the soccer team for two years and didn’t want to leave her team. She would not be sleeping in her bedroom, which she loved and had decorated all by herself. She just hated the whole thing. Adie likes cheese.";
+
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{"role": "system", "content": "You are a summarizer."}, {role: "user", content: toSummarize}, ],
+    });
+    console.log(completion.data.choices[0].message?.content);
     
-    console.log("List of Models:", response.data);
   } catch (error) {
     console.error("Error:", error);
   }
 }
 
-listModels();
+connectAPI();
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Box, Button, TextField} from '@mui/material';
 
 function App() {
+
+  const handleSubmit = async () => {
+    let toSummarize: string = "Addie had lived in Happyville since she was born. Next week, however, Addie and her family were moving over 1,000 miles away to Washington. Addie despised the idea of moving for many reasons. She was sad to be leaving her best friend. She had played on the soccer team for two years and didn’t want to leave her team. She would not be sleeping in her bedroom, which she loved and had decorated all by herself. She just hated the whole thing. Adie likes cheese.";
+
+    const options = {
+      method: 'POST', 
+      headers: {
+        'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify ({
+        model: "gpt-3.5-turbo",
+        messages: [{"role": "system", "content": "You are a summarizer."}, {role: "user", content: toSummarize}, ],
+      })
+    }
+    try {
+      const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+      const data = await response.json();
+      console.log(data.choices[0].message?.content); 
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Box className='inputBox'>
+            <TextField type="text" id="userInput" name="userInput"/>
+            <Button type="submit" value="Submit" onClick={() => handleSubmit()}> Submit </Button>
+        </Box>
       </header>
     </div>
   );
