fe0088a4ebf1c80411fd0de77d11142458661d47
NateGaNe <81611522+NathanKong06@users.noreply.github.com>
2023-08-20 18:15:52 -0700

    Added Summarizer Logo, loading icon for summaries

--- frontend/src/components/switch/switch.tsx
+++ frontend/src/components/switch/switch.tsx
@@ -1,4 +1,4 @@
-// import * as React from 'react';
+import * as React from 'react';
 // import { styled } from '@mui/material/styles';
 // import FormGroup from '@mui/material/FormGroup';
 // import FormControlLabel from '@mui/material/FormControlLabel';
@@ -6,7 +6,13 @@
 // import Stack from '@mui/material/Stack';
 // import Typography from '@mui/material/Typography';
 
+export {}
 // export const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+
+//     return (
+
+//    );
+//     }));
 //   width: 62,
 //   height: 34,
 //   padding: 7,
@@ -53,4 +59,4 @@
 //   },
 // }));
 
-//export default MaterialUISwitch;
+// export default MaterialUISwitch;
